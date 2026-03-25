6e8a1c7a4697d9b01c8e1ad6bdab9df4460d0a2a
Lyn <lyn250904@gmail.com>
2026-03-26 01:40:54 +0700

    feat : Quản lý danh mục

--- resolvers.ts
+++ resolvers.ts
@@ -1,10 +1,7 @@
 import Article from "./models/article.model";
+import Category from "./models/category.model";
 
-interface GetArticleArgs {
-    id: string;
-}
-
-interface DeleteArticleArgs {
+interface IdArgs {
     id: string;
 }
 
@@ -13,13 +10,28 @@ interface ArticleInput {
     avatar?: string;
     description?: string;
 }
+
+interface CreateArticleArgs {
+    article: ArticleInput;
+}
+
 interface UpdateArticleArgs {
     id: string;
     article: ArticleInput;
 }
 
-interface CreateArticleArgs {
-    article: ArticleInput;
+interface CategoryInput {
+    title?: string;
+    avatar?: string;
+}
+
+interface CreateCategoryArgs {
+    category: CategoryInput;
+}
+
+interface UpdateCategoryArgs {
+    id: string;
+    category: CategoryInput;
 }
 
 export const resolvers = {
@@ -28,18 +40,32 @@ export const resolvers = {
             return "Hello World!";
         },
         getListArticle: async () => {
-            const article = await Article.find({
+            const articles = await Article.find({
                 deleted: false
             });
-            return article;
+            return articles;
         },
-        getArticle: async (_: unknown, args: GetArticleArgs) => {
+        getArticle: async (_: unknown, args: IdArgs) => {
             const { id } = args;
             const article = await Article.findOne({
                 _id: id,
                 deleted: false
             });
             return article;
+        },
+        getListCategory: async () => {
+            const categories = await Category.find({
+                deleted: false
+            });
+            return categories;
+        },
+        getCategory: async (_: unknown, args: IdArgs) => {
+            const { id } = args;
+            const category = await Category.findOne({
+                _id: id,
+                deleted: false
+            });
+            return category;
         }
     },
     Mutation: {
@@ -49,7 +75,22 @@ export const resolvers = {
             await record.save();
             return record;
         },
-        deleteArticle: async (_: unknown, args: DeleteArticleArgs) => {
+        updateArticle: async (_: unknown, args: UpdateArticleArgs) => {
+            const { id } = args;
+            const { article } = args;
+            const record = await Article.findOneAndUpdate(
+                {
+                    _id: id,
+                    deleted: false
+                },
+                article,
+                {
+                    new: true
+                }
+            );
+            return record;
+        },
+        deleteArticle: async (_: unknown, args: IdArgs) => {
             const { id } = args;
             await Article.updateOne(
                 {
@@ -62,19 +103,38 @@ export const resolvers = {
             );
             return "Da xoa";
         },
-        updateArticle: async (_: unknown, args: UpdateArticleArgs) => {
-            const { id, article } = args;
-            const record = await Article.findOneAndUpdate(
+        createCategory: async (_: unknown, args: CreateCategoryArgs) => {
+            const { category } = args;
+            const record = new Category(category);
+            await record.save();
+            return record;
+        },
+        updateCategory: async (_: unknown, args: UpdateCategoryArgs) => {
+            const { id, category } = args;
+            const record = await Category.findOneAndUpdate(
                 {
                     _id: id,
                     deleted: false
                 },
-                article,
+                category,
                 {
                     new: true
                 }
             );
             return record;
+        },
+        deleteCategory: async (_: unknown, args: IdArgs) => {
+            const { id } = args;
+            await Category.updateOne(
+                {
+                    _id: id
+                },
+                {
+                    deleted: true,
+                    deletedAt: new Date()
+                }
+            );
+            return "Da xoa";
         }
     }
 };
