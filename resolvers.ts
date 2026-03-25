e985f8616fec8ce917d74d0e0001460842f1bb36
Lyn <lyn250904@gmail.com>
2026-03-26 01:29:36 +0700

    feat : Xóa mềm article

--- resolvers.ts
+++ resolvers.ts
@@ -4,6 +4,20 @@ interface GetArticleArgs {
     id: string;
 }
 
+interface DeleteArticleArgs {
+    id: string;
+}
+
+interface ArticleInput {
+    title?: string;
+    avatar?: string;
+    description?: string;
+}
+
+interface CreateArticleArgs {
+    article: ArticleInput;
+}
+
 export const resolvers = {
     Query: {
         hello: () => {
@@ -22,6 +36,27 @@ export const resolvers = {
                 deleted: false
             });
             return article;
+        }
+    },
+    Mutation: {
+        createArticle: async (_: unknown, args: CreateArticleArgs) => {
+            const { article } = args;
+            const record = new Article(article);
+            await record.save();
+            return record;
+        },
+        deleteArticle: async (_: unknown, args: DeleteArticleArgs) => {
+            const { id } = args;
+            await Article.updateOne(
+                {
+                    _id: id
+                },
+                {
+                    deleted: true,
+                    deletedAt: new Date()
+                }
+            );
+            return "Da xoa";
         }
     }
 };
